c36b61658b2b3fa30c262a494903bae38cec9c15
Talinshv <talinshahvaladian@gmail.com>
2026-05-05 23:08:52 +0400

    final project normalization

--- queries/final_project/normalization.sql
+++ queries/final_project/normalization.sql
@@ -66,7 +66,25 @@ WHERE "itemLocation" IS NOT NULL;
 
 SELECT * FROM analytics.location LIMIT 10;
 
--- 9.  Create the Gender Table
+-- 9. Add city, state, country as separate columns
+ALTER TABLE analytics.location
+ADD COLUMN city    VARCHAR(255),
+ADD COLUMN state   VARCHAR(255),
+ADD COLUMN country VARCHAR(255);
+
+-- 10. Split full location into the 3 new columns
+UPDATE analytics.location
+SET
+    city    = TRIM(SPLIT_PART(full_location, ',', 1)),
+    state   = TRIM(SPLIT_PART(full_location, ',', 2)),
+    country = TRIM(SPLIT_PART(full_location, ',', 3));
+
+SELECT full_location, city, state, country
+FROM analytics.location
+LIMIT 10;
+
+
+-- 11.  Create the Gender Table
 CREATE TABLE analytics.gender (
     gender_id   SERIAL PRIMARY KEY,
     gender_name VARCHAR(50) NOT NULL UNIQUE
@@ -79,7 +97,7 @@ WHERE gender IS NOT NULL;
 
 SELECT * FROM analytics.gender LIMIT 10;
 
--- 10. create main table
+-- 12. create main table
 CREATE TABLE analytics.fact_perfume_listing (
     listing_id    SERIAL PRIMARY KEY,
     title         VARCHAR(500),
@@ -93,7 +111,7 @@ CREATE TABLE analytics.fact_perfume_listing (
     gender_id     INT REFERENCES analytics.gender(gender_id)
 );
 
--- 11. Fill the Fact Table from Your Raw Data
+-- 13. Fill the Fact Table from Your Raw Data
 INSERT INTO analytics.fact_perfume_listing (
     title, price, available, sold, last_updated,
     brand_id, type_id, location_id, gender_id
@@ -102,12 +120,12 @@ SELECT
     s.title,
     s.price,
     s.available,
-    s.sold,
+    s.sold::INT,
     CASE
-    	WHEN s."lastUpdated" ~ '^\d' 
-    	THEN NULL
-    	ELSE TO_TIMESTAMP(s."lastUpdated", 'Mon DD, YYYY HH24:MI:SS TZ')
-	END,
+        WHEN s."lastUpdated" ~ '^\d'
+        THEN NULL
+        ELSE TO_TIMESTAMP(s."lastUpdated", 'Mon DD, YYYY HH24:MI:SS TZ')
+    END,
     b.brand_id,
     t.type_id,
     l.location_id,
@@ -118,7 +136,72 @@ LEFT JOIN analytics.fragrance_type t ON t.type_name     = s.type
 LEFT JOIN analytics.location       l ON l.full_location = s."itemLocation"
 LEFT JOIN analytics.gender         g ON g.gender_name   = s.gender;
 
---12. Verify Everything Worked
+--14. Verify Everything Worked 
 SELECT 'staging'    AS source, COUNT(*) FROM analytics._stg_perfume
 UNION ALL
 SELECT 'fact_table' AS source, COUNT(*) FROM analytics.fact_perfume_listing;
+
+SELECT * FROM analytics.location LIMIT 10;
+
+--Questions:
+--1.Which brands have the most listings?(counts how many rows (listings) exist per brand)
+SELECT
+    b.brand_name,
+    COUNT(*) AS total_listings
+FROM analytics.fact_perfume_listing f
+JOIN analytics.brand b ON b.brand_id = f.brand_id
+GROUP BY b.brand_name
+ORDER BY total_listings DESC
+LIMIT 10;
+
+--2.Which brand has the highest average price?
+SELECT
+    b.brand_name,
+    ROUND(AVG(f.price), 2) AS avg_price
+FROM analytics.fact_perfume_listing f
+JOIN analytics.brand b ON b.brand_id = f.brand_id
+GROUP BY b.brand_name
+ORDER BY avg_price DESC
+LIMIT 10;
+
+--3.Which brand has sold the most units in total?
+SELECT
+    b.brand_name,
+    SUM(f.sold) AS total_units_sold
+FROM analytics.fact_perfume_listing f
+JOIN analytics.brand b ON b.brand_id = f.brand_id
+WHERE f.sold IS NOT NULL
+GROUP BY b.brand_name
+ORDER BY total_units_sold DESC
+LIMIT 10;
+
+--4. Top 10 best selling perfumes by units sold?
+SELECT
+    b.brand_name,
+    f.title,
+    f.sold
+FROM analytics.fact_perfume_listing f
+JOIN analytics.brand b ON b.brand_id = f.brand_id
+WHERE f.sold IS NOT NULL
+ORDER BY f.sold DESC
+LIMIT 10;
+
+-- 5.Average price per fragrance type?
+SELECT
+    t.type_name,
+    ROUND(AVG(f.price), 2) AS avg_price,
+    COUNT(*)               AS total_listings
+FROM analytics.fact_perfume_listing f
+JOIN analytics.fragrance_type t ON t.type_id = f.type_id
+GROUP BY t.type_name
+ORDER BY avg_price DESC;
+
+SELECT
+    b.brand_name,
+    COUNT(*)               AS total_listings,
+    ROUND(AVG(f.price), 2) AS avg_price
+FROM analytics.fact_perfume_listing f
+JOIN analytics.brand b ON b.brand_id = f.brand_id
+GROUP BY b.brand_name
+ORDER BY total_listings DESC
+LIMIT 10;
